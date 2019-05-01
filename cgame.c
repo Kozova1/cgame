379a98b6eb26597dd87c80ebcd0bc7456268699a
Kozova1 <mug66kk@gmail.com>
2019-05-01 20:12:04 +0300

    added input interface

--- cgame.c
+++ cgame.c
@@ -1,1 +1,16 @@
-#include "./config.h"
+#include <stdio.h>
+#include "./config.def.h"
+
+char * get_input() {
+    char *c;
+    printf("> ");
+    scanf("> %s", c);
+    return c;
+}
+
+int main() {
+    printf("%s v%s\n\n", name, version);
+    char *c = get_input();
+
+    return 0;
+}
