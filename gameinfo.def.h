a12291126fa6e26abf87520c4ad72063b96df247
Kozova1 <mug66kk@gmail.com>
2019-05-01 12:56:34 +0300

    added basic config file

---
+++ gameinfo.def.h
@@ -0,0 +1,17 @@
+/* Version of the game */
+static char version[] = "";
+
+/* Name of the game */
+static char name[] = "";
+
+/* rows and columns of the map.
+ * to create an empty space make the square have no entrances.
+ * */
+#define columns 1
+#define rows 1
+
+/* room at which to spawn
+ * on a grid of 3x3:
+ * room 0 is upper left, room 1 is upper middle, etc...
+ * */
+static int spawnroom = 0;
